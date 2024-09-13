127bc8eba018778d967dade1a588f643712471e6
kyaw swar lynn <kyawswarlynn14@gmail.com>
2024-09-13 13:18:42 +0630

    add filter func

--- src/pages/Outcomes.tsx
+++ src/pages/Outcomes.tsx
@@ -15,15 +15,38 @@ import { db } from "@/lib/firebase";
 import { toast } from "@/hooks/use-toast";
 import { changeTimestampToDate, displayDate, getCategoryName } from "@/lib/services";
 import MonthYearPicker from "@/components/MonthYearPicker";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import OutcomeDetail from "@/components/OutcomeDetail";
 import OutcomeForm from "@/components/OutcomeForm";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { TOutcome } from "@/types";
 
 const Incomes = () => {
   const date = new Date();
-  const { outcomeLoading, outcomes, categories } = useData();
+  const { outcomeLoading, outcomes, categories, categoryLoading } = useData();
   const [month, setMonth] = useState(date.getMonth());
   const [year, setYear] = useState(date.getFullYear());
+  const [showAll, setShowAll] = useState(false);
+  const [categoryId, setCategoryId] = useState('');
+  const [filteredOutcomes, setFilteredOutcomes] = useState<TOutcome[]>([]);
+
+  useEffect(() => {
+    if(showAll) {
+      if(categoryId && categoryId !== "-") {
+        setFilteredOutcomes(outcomes.filter(outcome => outcome.category === categoryId));
+      } else {
+        setFilteredOutcomes(outcomes);
+      }
+    } else {
+      const data = outcomes
+        .filter((outcome) => {
+          const outcomeDate = changeTimestampToDate(outcome.createdAt);
+          return outcomeDate.getMonth() === month && outcomeDate.getFullYear() === year;
+        });
+      setFilteredOutcomes(data);
+    }
+  }, [categoryId, outcomes, showAll, month, year]);
 
   const deleteOutcome = async (id: string) => {
     try {
@@ -35,24 +58,50 @@ const Incomes = () => {
     }
   };
 
-  const filteredOutcomes = outcomes
-    .filter((outcome) => {
-      const outcomeDate = changeTimestampToDate(outcome.createdAt);
-      return outcomeDate.getMonth() === month && outcomeDate.getFullYear() === year;
-    })
-    .sort((a, b) => {
-      const dateA = changeTimestampToDate(a.createdAt);
-      const dateB = changeTimestampToDate(b.createdAt);
-      return dateB.getTime() - dateA.getTime();
-    });
-
   const totalAmount = filteredOutcomes.reduce((a, b) => a + Number(b.amount), 0)
 
   return (
     <div className="w-[98%] md:w-[90%] lg:w-[80%]  mx-auto">
       <div className="py-4 flex justify-between items-center">
         <OutcomeForm />
-        <MonthYearPicker month={month} setMonth={setMonth} year={year} setYear={setYear} />
+        <div className="flex items-center space-x-2">
+          {showAll ? (
+            <Select 
+              disabled={categoryLoading} 
+              value={categoryId}
+              onValueChange={value => setCategoryId(value)}
+            >
+              <SelectTrigger className="w-[150px]">
+                <SelectValue
+                  placeholder={
+                    categoryLoading ? "Loading..." : "Select a category"
+                  }
+                />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectItem value={"-"}>All</SelectItem>
+                  {categories.length > 0 &&
+                    categories.map((i) => (
+                      <SelectItem key={i.id} value={i.id}>
+                        {i.title}
+                      </SelectItem>
+                    ))}
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          ) : (
+            <MonthYearPicker month={month} setMonth={setMonth} year={year} setYear={setYear} />
+          )}
+
+          <Checkbox id="showAll" checked={showAll} onCheckedChange={() => setShowAll(prev => !prev)} />
+          <label
+            htmlFor="showAll"
+            className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+          >
+            Show all?
+          </label>
+        </div>
       </div>
 
       <Table className="border border-slate-300 shadow-lg">
@@ -76,7 +125,11 @@ const Incomes = () => {
           </TableBody>
         ) : (
           <TableBody>
-            {filteredOutcomes.map((i) => (
+            {filteredOutcomes.sort((a, b) => {
+              const dateA = changeTimestampToDate(a.createdAt);
+              const dateB = changeTimestampToDate(b.createdAt);
+              return dateA.getTime() - dateB.getTime();
+            }).map((i) => (
               <TableRow key={i.id}>
                 <TableCell className="font-medium">{i.title}</TableCell>
                 <TableCell>{Number(i.amount).toLocaleString()} MMK</TableCell>
